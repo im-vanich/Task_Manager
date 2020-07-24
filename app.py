706dfd837ec71a9d3768fa4c5f8abccc6d501a19
Ivan Matveev <skiforceone@gmail.com>
2020-07-24 19:48:36 +0300

    Finished work with task manager

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -11,17 +11,40 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DATETIME, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect('/')
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
